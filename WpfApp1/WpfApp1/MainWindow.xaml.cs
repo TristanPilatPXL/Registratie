1a1d61f3d6b5386ec2c85214ba90519aad066c0c
TristanPilatPXL <Tristan.Pilat@STUDENT.PXL.BE>
2025-12-14 19:10:12 +0100

    REGI-02:labels, listbox en button aangemaakt. cs code info voor lijst aan te vullen

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -1,24 +1,60 @@
-﻿using System.Text;
+﻿using System.Collections.Generic;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+        using System.Windows.Data;
+        using System.Windows.Documents;
+        using System.Windows.Input;
+        using System.Windows.Media;
+        using System.Windows.Media.Imaging;
+        using System.Windows.Navigation;
+        using System.Windows.Shapes;
+        using WpfApp1.Models;
 
-namespace WpfApp1
-{
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
-    {
-        public MainWindow()
+        namespace WpfApp1
         {
-            InitializeComponent();
+            /// <summary>
+            /// Interaction logic for MainWindow.xaml
+            /// </summary>
+            public partial class MainWindow : Window
+            {
+                public MainWindow()
+                {
+                    InitializeComponent();
+                    LoadOlodsInListBox();
+                }
+
+                private void LoadOlodsInListBox()
+                {
+                    var olods = CreateOlodList();
+                    olodListBox.ItemsSource = olods;
+                }
+
+        private List<Olod> CreateOlodList()
+        {
+            return new List<Olod>()
+    {
+        new Olod() {
+            Name = "C# Essentials",
+            Credits = 7
+        },
+        new Olod() {
+            Name = "C# Advanced",
+            Credits = 6
+        },
+        new Olod() {
+            Name = "C# Web1",
+            Credits = 7
+        },
+        new Olod() {
+            Name = "C# Mobile",
+            Credits = 6
+        },
+        new Olod() {
+            Name = "C# Web2",
+            Credits = 4
+        },
+    };
         }
     }
-}
+        }
